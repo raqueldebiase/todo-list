e5124ac1eab7d41f53bfc6069ce7dca1e78d2ed9
Raquel De Biase <raqueldebiase@gmail.com>
2022-09-24 10:57:14 +0100

    Fix: usando useState na função

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,30 +1,19 @@
-
 import './App.css';
 import Botao from './Botao';
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
-class Numero extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
+function Numero(){
+  const [count, setCount] = useState (0)
 
-  componentDidUpdate(){
-    document.title = `Clicou ${this.state.count} vezes`
-  }
+  return(
+    <div className='App'>
+    <p>Clicou {count} vezes</p>
+    <Botao titulo='Incrementar' onClickHandler={() => setCount (count+1)}></Botao>
+    </div>
+  )
 
-  render (){
-    return (
-      <div className="App">
-      <p>Clicou {this.state.count} vezes</p>
-      <Botao titulo="+" onClickHandler={() => this.setState({count: this.state.count+1})}></Botao>
-      </div>
-    );
-  }
 }
 
 export default Numero;
